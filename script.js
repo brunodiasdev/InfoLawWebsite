68baae92b9dbbb6228a76240816308d28e9455a1
brunodiasdev <brunodiasdevs@gmail.com>
2020-12-27 17:05:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function imprimir (item, i, cas){
     console.log(`${i + 1}, ${item}`)
 }
 n1.forEach (imprimir)
-n1.forEach(function(a, b){
-    console.log(a, b)
+n1.forEach(function(a, b, c){
+    console.log(a, b, c)
 })
