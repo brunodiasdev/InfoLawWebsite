3f272c96454998a15cdf0366ade27f36cac9c84e
brunodiasdev <brunodiasdevs@gmail.com>
2020-12-30 20:49:28 -0300

    Update script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-function triangulo (l1, l2, l3){
-    if (l1 === l2 && l2 === l3){
-        return `É um triangulo equilátero: Lado 1 (${l1}) - Lado 2 (${l2}) - Lado 3 (${l3}) `
-    } else if (l1!== l2 && l2 !== l3 && l3!==l1){
- 
