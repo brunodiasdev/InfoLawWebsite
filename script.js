5b1b41f67fce0e7a93c532b56fad5cc41358eac2
brunodiasdev <brunodiasdevs@gmail.com>
2021-01-04 16:45:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function teste (num, div = 3 ){
     if (num % div){
-        console.log(`${num} é divisível por ${div}`)
-    } 
+        console.log(`${num} é divisível por ${div}!`)
+    } else{
+        console.log(`OPS! ${num} Não é divisível por ${div}!`)
+    }
 }
